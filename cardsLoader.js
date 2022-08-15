00baa9ed704e4d66927ebf666d6f3bb09ac00cd1
cliknik <cliknik13@gmail.com>
2022-08-15 21:45:36 +0300

    add card loader

---
+++ cardsLoader.js
@@ -0,0 +1,36 @@
+const $elements = document.querySelector('.elements');
+let $element = $elements.querySelectorAll('.elements__card');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+function loadCards() {
+    $elements.innerHTML = initialCards.map(card => ` <li class="elements__card">
+          <img src="${card.link}" class="elements__image" alt="Фото ${card.name}">
+          <h2 class="elements__description">${card.name}</h2>
+          <button class="elements__like elements__like_status_inactive" type="button"></button>
+        </li>`).join('');
+}
+loadCards();
