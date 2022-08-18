85eb515fbacfe99aff780680508b3ed8b2f678d0
cliknik <cliknik13@gmail.com>
2022-08-18 22:33:40 +0300

    add delete btn

--- cardsLoader.js
+++ cardsLoader.js
@@ -26,11 +26,14 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
+
 function loadCards() {
     $elements.innerHTML = initialCards.map(card => ` <li class="elements__card">
+          <button class="elements__delete-item" type="button"></button>
           <img src="${card.link}" class="elements__image" alt="Фото ${card.name}">
           <h2 class="elements__description">${card.name}</h2>
           <button class="elements__like elements__like_status_inactive" type="button"></button>
         </li>`).join('');
+
 }
 loadCards();
